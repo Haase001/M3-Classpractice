b893637e65165792317cee7a9ec93c8997bebb50
NIBI <haase.sergio1@gmail.com>
2025-02-13 17:39:56 -0600

    Primer commit creacion de archivos html y js

---
+++ index.js
@@ -0,0 +1,16 @@
+//console.log('Hola mundo!')
+
+//alert('Estás siendo espiado xD')
+
+//var nombre = 'Haase'
+//var edad = 29
+//var gusto1 = 'Escuchar musica'
+//var gusto2 = 'Correr'
+
+//console.log('Mi nombre es:', nombre, 'tengo la edad de', edad, 'me gusta', gusto1, 'y tambien', gusto2)
+
+//console.log(`Mi nombre es ${nombre}`)
+
+for(let index = 1; index < 100; index ++) {
+    console.log(index);
+}
